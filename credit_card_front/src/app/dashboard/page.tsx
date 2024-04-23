6f333a914d1a59ac033c80925e8b60bb1018ad03
lhenriquedev <sousarodriguesluishenrique@gmail.com>
2024-04-23 15:28:39 -0300

    feat: trabalhando na tela de login

---
+++ credit_card_front/src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <div>Dashboard</div>
+    </main>
+  );
+}
--- credit_card_front/tailwind.config.ts
+++ credit_card_front/tailwind.config.ts
@@ -7,14 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
+  plugins: [require("daisyui")],
 };
 export default config;
